00855662db8629fd28c531d606e6d5ca945ccbe3
pabloes <estornut@gmail.com>
2024-10-09 20:05:05 +0200

    implementation example with 4 different browsers at same time

--- test-projects/implementation/src/index.ts
+++ test-projects/implementation/src/index.ts
@@ -1,9 +1,10 @@
 import {  Vector3, Quaternion } from "@dcl/sdk/math";
 import {createVirtualBrowserClient} from "@zeroxwork/decentraland-virtual-browser-client";
 
-const SERVER_BASE_URL = "http://localhost:3000";
-const WEBSOCKET_URL = "ws://localhost:3000";
-const POSITION = [8, 2, 8];
+/*const SERVER_BASE_URL = "http://localhost:3000";
+const WEBSOCKET_URL = "ws://localhost:3000";*/
+
+
 const _logs = console.log;
 
 console.log = (...args: any[]) => {
@@ -12,14 +13,41 @@ console.log = (...args: any[]) => {
   _logs(`BRO:`, time, ...args);
 };
 
+
+const SERVER_BASE_URL = "https://dcl-browser.zeroxwork.com";
+const WEBSOCKET_URL = "wss://dcl-browser.zeroxwork.com";
 export async function main() {
-  const virtualBrowserClient = createVirtualBrowserClient({
-    position:Vector3.create(...POSITION),
-    rotation:Quaternion.fromEulerDegrees(0,0,0),
+
+  createVirtualBrowserClient({
     colyseusServerURL:WEBSOCKET_URL,
     baseAPIURL:SERVER_BASE_URL,
+    position:Vector3.create(2, 2, 8),
+    rotation:Quaternion.fromEulerDegrees(0,0,0),
     scale:Vector3.create(3,768/1024 * 3,1),
-    resolution:[1024,768]
-    //homeURL:"https://cardgames.io/"
+    homeURL:"https://decentraland.org/governance"
+  });
+  createVirtualBrowserClient({
+    colyseusServerURL:WEBSOCKET_URL,
+    baseAPIURL:SERVER_BASE_URL,
+    position:Vector3.create(5.2, 2, 8),
+    rotation:Quaternion.fromEulerDegrees(0,0,0),
+    scale:Vector3.create(3,768/1024 * 3,1),
+    homeURL:"https://cocobay.world"
+  });
+  createVirtualBrowserClient({
+    colyseusServerURL:WEBSOCKET_URL,
+    baseAPIURL:SERVER_BASE_URL,
+    position:Vector3.create(8.4, 2, 8),
+    rotation:Quaternion.fromEulerDegrees(0,0,0),
+    scale:Vector3.create(3,768/1024 * 3,1),
+    homeURL:"https://golfcraftgame.com/tournaments"
+  });
+  createVirtualBrowserClient({
+    colyseusServerURL:WEBSOCKET_URL,
+    baseAPIURL:SERVER_BASE_URL,
+    position:Vector3.create(11.6, 2, 8),
+    rotation:Quaternion.fromEulerDegrees(0,0,0),
+    scale:Vector3.create(3,768/1024 * 3,1),
+    homeURL:"https://cardgames.io"
   });
 }
