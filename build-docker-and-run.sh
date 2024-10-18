69b998f8a7439c945ed230f931a4fdd3effcc15d
pabloes <estornut@gmail.com>
2024-10-18 10:43:42 +0200

    setup .docker.env

--- build-docker-and-run.sh
+++ build-docker-and-run.sh
@@ -5,6 +5,6 @@ docker build -t dcl-browser .
 docker stop dcl-browser-container
 docker rm dcl-browser-container
 # Run the container based on the built image
-docker run --env-file .env --rm -d --name dcl-browser-container -p 3000:3000 dcl-browser
+docker run --env-file .docker.env --rm -d --name dcl-browser-container -p 3000:3000 dcl-browser
 
 docker logs dcl-browser-container --follow
